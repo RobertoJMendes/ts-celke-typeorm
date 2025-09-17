350a750c4eb1c121c8441a26a10c97e8707c778c
RobertoJMendes <r0bert0.mendes@yahoo.com.br>
2025-09-17 13:43:45 -0300

    adicionado cors

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,13 @@
 import express, {} from 'express';
+import cors from 'cors';
 import { AppDataSource } from './data-source.js';
 import userControllers from './controllers/user.js';
 const ap = express();
 ap.use(express.json());
+ap.use(cors());
 ap.use('/', userControllers);
 AppDataSource.initialize() // teste conexão com DB!
     .then(() => { console.log("ok! - db"); }).catch((error) => { console.log(error); });
-ap.listen(3000, () => { console.log("3000 -> ok!"); });
-//ap.get("/",( req:Request, res:Response )=>{ res.send( "Oi, legal!" ) })
+ap.listen(4000, () => { console.log("4000 -> ok!"); });
+//ap.get("/",( req:Request, res:Response )=>{ res.send( "Oi, legal!" )
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { type Request, type Response } from 'express'
+import cors from 'cors'
 import {AppDataSource} from './data-source.js'
 import userControllers from './controllers/user.js'
 const ap = express()
 ap.use(express.json())
+ap.use(cors())
 ap.use('/', userControllers)
 AppDataSource.initialize() // teste conexão com DB!
 .then(()=>{ console.log("ok! - db") }).catch((error)=>{ console.log(error) })
-ap.listen(3000,()=>{ console.log( "3000 -> ok!" ) })
-//ap.get("/",( req:Request, res:Response )=>{ res.send( "Oi, legal!" ) })
+ap.listen(4000,()=>{ console.log( "4000 -> ok!" ) })
+//ap.get("/",( req:Request, res:Response )=>{ res.send( "Oi, legal!" )
